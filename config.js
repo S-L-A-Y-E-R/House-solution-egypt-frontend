e9abe149891317c4a9ff24e8a3ba2037e7cd39e3
S-L-A-Y-E-R <elshehry2000@gmail.com>
2024-03-21 01:02:46 +0200

    some fixes

--- config.js
+++ config.js
@@ -8,8 +8,10 @@
 // export const API_BASE_URL = 'http://localhost:8000/api';
 // export const PROPERTY_BASE_URL = `http://localhost:8000/api/uploads/properties/`
 // export const BLOG_IMAGE_BASE_URL = `http://localhost:8000/api/uploads/blogs/`
+// export const WEBSITE_BASE_URL = "http://localhost:3000";
 
-// export const API_BASE_URL = 'https://housepoint-nodejs-backend.onrender.com';
+
+// export const API_BASE_URL = 'https://housepoint-nodejs-backend.onrender.com';S
 export const WEBSITE_BASE_URL = "https://housepointegypt.com";
 export const API_BASE_URL = "https://housepointegypt.com/api/api";
 export const PROPERTY_BASE_URL = `https://housepointegypt.com/api/api/uploads/properties/`;
